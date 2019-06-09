ab88931fab7b30240938f2a364e7d213ba1c9863
Daniel Bogucki <daniel.bogucki95@gmail.com>
2019-06-09 17:31:59 +0200

    common functionality in Main implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,17 @@ import com.dbogucki.bulbapi.exceptions.DeviceNotRecognizedException;
 import com.dbogucki.bulbapi.exceptions.DeviceSocketException;
 import com.dbogucki.bulbapi.exceptions.ResultException;
 import com.dbogucki.bulbapi.factories.BulbFactory;
-import com.dbogucki.bulbapi.results.Result;
 
 import java.io.IOException;
 
-import java.util.Map;
 import java.util.Set;
 
 public class Main {
 
     public static String lastValue;
 
-    public static void main(String[] args) throws DeviceNotRecognizedException, DeviceSocketException, ResultException {
-        if (args[0] != null)
+    public static void main(String[] args) throws DeviceNotRecognizedException, DeviceSocketException, ResultException, IOException {
+        if (args.length >= 1) {
             switch (args[0]) {
                 case "help":
                     System.out.println("Current list of commands: ");
@@ -30,32 +28,35 @@ public class Main {
                 case "discover":
                     YeelightDiscoverer discoverer = new YeelightDiscoverer();
                     System.out.println("Discovering devices");
-                    try {
-                        Set<Bulb> set = discoverer.search();
-                        System.out.println("Devices found: " + set.size());
-                        for (Bulb b : set) {
-                            System.out.println("---------");
-                            System.out.println("Address: " + b.getIp() + ":" + b.getPort());
-                            System.out.println("Power: " + (b.getPower() ? "On" : "Off"));
-                            System.out.println("ColorMode: " + b.getColorMode());
-                            System.out.println("Brightness: " + b.getBright() + "%");
-                            System.out.println("White temperature: " + b.getColorTemperature() + "K");
-                            System.out.println("RGB value: " + b.getRGBvalue());
-                            System.out.println("HUE value: " + b.getHUEvalue());
-                            System.out.println();
-                            b.setPower(true);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (DeviceSocketException e) {
-                        e.printStackTrace();
-                    } catch (ResultException e) {
-                        e.printStackTrace();
+
+                    Set<Bulb> set = discoverer.search();
+                    System.out.println("Devices found: " + set.size());
+                    for (Bulb b : set) {
+                        System.out.println("---------");
+                        System.out.println("Address: " + b.getIp() + ":" + b.getPort());
+                        System.out.println("Power: " + (b.getPower() ? "On" : "Off"));
+                        System.out.println("ColorMode: " + b.getColorMode());
+                        System.out.println("Brightness: " + b.getBright() + "%");
+                        System.out.println("White temperature: " + b.getColorTemperature() + "K");
+                        System.out.println("RGB value: " + b.getRGBvalue());
+                        System.out.println("HUE value: " + b.getHUEvalue());
+                        System.out.println();
+                        b.setPower(true);
                     }
                     break;
                 case "toggle":
-                    if (args[1] != null && args[2] != null && args[3] != null) {
-                        Bulb bulb = BulbFactory.getBulb(Category.valueOf(args[3]), args[1], Integer.parseInt(args[2]));
+                    if (args.length >= 4) {
+                        Category category = Category.YEELIGHT;
+                        switch (args[3]) {
+                            case "Yeelight":
+                                category = Category.YEELIGHT;
+                                break;
+                            case "Default":
+                                /* falls through */
+                            default:
+                                category = Category.DEFAULT;
+                        }
+                        Bulb bulb = BulbFactory.getBulb(category, args[1], Integer.parseInt(args[2]));
                         bulb.toggle();
                     } else
                         System.out.println("Bad parameters. Should be {ip port type}");
@@ -64,7 +65,13 @@ public class Main {
                 default:
                     System.out.println("Unknown Command, please type \"help\"");
             }
-
+        } else {
+            System.out.println("This is common API to communicate with Yeelight bulbs");
+            System.out.println("type help to get list of supported commands in standalone version");
+            System.out.println("Author: Daniel Bogucki");
+        }
     }
 
 }
+
+
